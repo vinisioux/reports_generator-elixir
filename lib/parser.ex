78f4751e259b2fe5eb470380e8f53b45d02f2420
ViniciusLeonadovky <vile.pb@gmail.com>
2021-07-12 23:53:46 -0300

    initial commit

---
+++ lib/parser.ex
@@ -0,0 +1,17 @@
+defmodule ReportsGenerator.Parser do
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    # mesma coisa da linha de baixo
+    |> List.update_at(2, &String.to_integer/1)
+
+    # |> List.update_at(2, fn item -> String.to_integer(item) end) # mesma coisa da linha de cima
+  end
+end
